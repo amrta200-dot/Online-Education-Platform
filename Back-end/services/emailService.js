0f3eea5c434cdd36a461c75c55582a46eb3a114e
Amr <amr.ta200@gmail.com>
2026-09-02 20:25:48 +0300

    Fix Gmail SMTP connection

--- Back-end/services/emailService.js
+++ Back-end/services/emailService.js
@@ -1,18 +1,20 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-    },
+  },
 });
 
 export async function sendVerificationEmail(email, code) {
-    await transporter.sendMail({
-        from: `"منصة فصلي" <${process.env.EMAIL_USER}>`,
-        to: email,
-        subject: "تحقق من حساب منصة فصلي الخاص بك",
-        text: `رمز التحقق الخاص بك هو: ${code}`,
-    });
+  await transporter.sendMail({
+    from: `"منصة فصلي" <${process.env.EMAIL_USER}>`,
+    to: email,
+    subject: "تحقق من حساب منصة فصلي الخاص بك",
+    text: `رمز التحقق الخاص بك هو: ${code}`,
+  });
 }
